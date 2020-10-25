858766a29817dae3730f6df1c261017b0724c79c
Arslan Qamar <ravian720@gmail.com>
2020-10-26 06:09:53 +1100

    Use Env variables

--- Notifier/notifier.py
+++ Notifier/notifier.py
@@ -1,10 +1,11 @@
 import requests
 from mongohelper import *
+from os import environ
 
 def telegram_bot_sendtext(bot_message):
     
-    bot_token = '1398043950:AAG20P_o3Z0hgssyrGK-4gPgTG-guiCTjAQ'
-    bot_chatID = '1185242210'
+    bot_token = environ.get('bot_token')
+    bot_chatID = environ.get('bot_chatid')
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
